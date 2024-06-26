42f5ef552e8bd954c29284b23a415cbab43b0a43
Komal Singh <komalsingh@Komals-MacBook-Air.local>
2024-06-26 17:20:26 +0530

    modified gitignore file

--- dist/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Claculator = void 0;
-function Claculator(a, b) {
-    return a + b;
-}
-exports.Claculator = Claculator;
